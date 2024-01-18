603d8d16b989b060c31ead8a9021a5a67f78469f
kknd6169 <115420143+kknd6169@users.noreply.github.com>
2024-01-18 03:54:45 +0000

    Test2

---
+++ Lab2/script.js
@@ -0,0 +1,23 @@
+// Access the div with the id 'output'
+const outputDiv = document.getElementById('output');
+
+// Log a message to the console
+console.log('Hello from the console!');
+
+
+
+//Test
+
+console.log('The first javascript file!');
+
+
+
+
+// var location = "home";
+// //var dd = "Friday";
+
+var result = (location == "home" && dd == "Friday") ? "I will go out." : "I will stay home.";
+
+console.log(result);
+
+outputDiv.innerText = result;
