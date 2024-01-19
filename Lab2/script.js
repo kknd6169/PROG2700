f170112b190ded3c35050f0fdebda7bac64ef638
kknd6169 <115420143+kknd6169@users.noreply.github.com>
2024-01-19 15:45:30 +0000

    Lab2 done!

--- Lab2/script.js
+++ Lab2/script.js
@@ -1,23 +1,51 @@
 // Access the div with the id 'output'
 const outputDiv = document.getElementById('output');
 
-// Log a message to the console
-console.log('Hello from the console!');
-
-
-
-//Test
-
+// Test
 console.log('The first javascript file!');
 
+function isDateInRange(inputDate) {
+    // Create a Date object from the input
+    let date = new Date(inputDate);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+        console.log("Invalid Date");
+        return false;
+    }
+
+    // Define the start and end dates of the range
+    let year = date.getFullYear();
+    let startDate = new Date(year, 0, 19); 
+    let endDate = new Date(year, 1, 19);  
+
+    // Check if the date is within the range
+    return date >= startDate && date <= endDate;
+}
+
+var myLocation = "Halifax"; 
+var travelLocation = "Costa Rica"
+var inputDate = "January 20, 2024";
+var airport = "YHZ";
+var airportNum = "(902) 873 - 4422";
+
+var resultLocation = (myLocation == "Halifax") ? airport + " " + airportNum : "I will stay home.";
+
+var result = ""; 
+
+if (isDateInRange(inputDate)) {
+   
+    console.log("I am traveling in " + travelLocation + "!");
+
+    result = "I am traveling in " + travelLocation + "!"; 
+} else {
+    console.log("I am home in " + myLocation + ".");
+
+    result = "I am home in " + myLocation + ".";
+}
 
 
+console.log(resultLocation);
 
-// var location = "home";
-// //var dd = "Friday";
+outputDiv.innerText = result; 
 
-var result = (location == "home" && dd == "Friday") ? "I will go out." : "I will stay home.";
-
-console.log(result);
-
-outputDiv.innerText = result;
